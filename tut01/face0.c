dcc49b26bb656196af68c5260873318f71e66cf4
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-09-16 13:31:11 +1000

    Added tutorial code

---
+++ tut01/face0.c
@@ -0,0 +1,20 @@
+// On Thursday 16th September
+// By H11C
+// 
+// Prints out a face in the format:
+//  ~ ~
+//  0 0
+//   o
+//   -
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+    
+    return 0;
+}
