897076a8dd64144072dbd3704145ffcb030eaae2
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-10-07 13:52:02 +1100

    Added changes from class and removed unnecessary comments

--- tut04/review.c
+++ tut04/review.c
@@ -1,34 +1,37 @@
+//
+// By anonymous author
+// On a certain date
+//
+// Scans 2 numbers, adds them together and prints their sum.
+// Prints which are even and which are odd.
+//
+
 #include <stdio.h>
 
 int main(void) {
-    // Naming variables
-    int num1;   // num1 is the first number the user types in
-    int num2;   // num2 is the second number the user types in
-    int sum;    // sum is the result of adding the first and second number
+    int num1;
+    int num2;
 
-    // Getting the numbers from the user
     scanf("%d", &num1);
     scanf("%d", &num2);
 
-    // Adding the two numbers together
+    int sum;
     sum = num1 + num2;
-
-    // Tell the user the result
     printf("Your numbers add up to %d\n", sum);
 
     // Check if its divisible by 2
-    if (sum %2 != 0) {
-    if(num1 %2 == 0) {
-    printf("The first number you've typed was even and the second number was odd\n");
-    } else if (num1 %2 != 0) {
-    printf("The first number you've typed was odd and the second number was even\n");
-    }
-    } else if (sum %2 == 0) {
-    if(num1 %2 == 0) {
-    printf("Both the numbers you've typed were even\n"); 
-    } else if (num1 %2 != 0) {
-    printf("Both the numbers you've typed were odd\n");
-    }
+    if (sum % 2 != 0) {
+        if (num1 % 2 == 0) {
+            printf("The first number you've typed was even and the second number was odd\n");
+        } else if (num1 % 2 != 0) {
+            printf("The first number you've typed was odd and the second number was even\n");
+        }
+    } else {
+        if (num1 % 2 == 0) {
+            printf("Both the numbers you've typed were even\n"); 
+        } else if (num1 % 2 != 0) {
+            printf("Both the numbers you've typed were odd\n");
+        }
     }
 
     return 0;
