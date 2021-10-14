b50d202c8ee17625893f59e5a4ac1e5bda063a7f
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-10-14 13:21:52 +1100

    Forgot valid_functions.c

--- tut05/valid_functions.c
+++ tut05/valid_functions.c
@@ -1,6 +1,6 @@
 //
-// Author: <class code>
-// Date: <date>
+// Author: H11C
+// Date: 14th October 2021
 //
 // Description:
 // Attempt to implement each of the functions that may or may not be valid
@@ -9,43 +9,35 @@
 
 #include <stdio.h>
 
-int array_length(int nums[]);
-int test_all_positive(int nums[]);
-int test_all_initialized(int length, int nums[]);
+
 int test_all_positive(int length, int nums[]);
+
 
 int main(void) {
 
     int first_array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("First array = %d\n", test_all_positive(10, first_array));
     int second_array[7] = {-6, 8, 1, -4, 3, 0, -4};
-
+    printf("Second array = %d\n", test_all_positive(7, second_array));
+    
     return 0;
 }
 
-// Gets the length of the array
-// Takes in an array
-// Returns the length of the array
-int array_length(int nums[]) {
-
-}
-
-// Tests all values in the array are positive
-// Takes in an array
-// Returns 1 if each value is positive and 0 if not
-int test_all_positive(int nums[]) {
-
-}
-
-// Tests all values in the array are initialised
-// Takes in an array and its length
-// Returns 1 if each value is initialised and 0 if not
-int test_all_initialized(int length, int nums[]) {
-
-}
 
 // Tests all values in the array are positive
 // Takes in an array and its length
 // Returns 1 if each value is positive and 0 if not
+// Note. 0 is a positive number
 int test_all_positive(int length, int nums[]) {
+    
+    int i = 0;
+    while (i < length) {
+        if (nums[i] <= -1) {
+            return 0;
+        }
+    
+        i++;
+    }
 
+    return 1;
 }
